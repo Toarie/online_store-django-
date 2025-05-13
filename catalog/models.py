8d5a43a6c653d6714abdf307de178b4a187105e0
Ulyana Kuznetsova <rey.tsvetkov777@gmail.com>
2025-05-13 17:11:17 +0300

    Мои текущие изменения перед слиянием

--- catalog/models.py
+++ catalog/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
     description = models.TextField(blank=True, verbose_name='Описание')
-    slug = models.SlugField(max_length=100, unique=True, blank=True)
 
     class Meta:
         verbose_name = 'Категория'
@@ -19,7 +18,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
-    image = models.ImageField(upload_to='products/', blank=True, verbose_name='Изображение')
+    image = models.ImageField(upload_to='products/', blank=True, null=True, verbose_name='Изображение')
     in_stock = models.BooleanField(default=True, verbose_name='В наличии')
 
     class Meta:
